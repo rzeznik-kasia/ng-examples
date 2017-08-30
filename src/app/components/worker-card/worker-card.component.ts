392827bde2d00925503c3137e4d5e2e5354db89e
rzeznik-kasia <rzeznik.kasia@gmail.com>
2017-08-30 14:01:40 +0200

    add service

--- src/app/components/worker-card/worker-card.component.ts
+++ src/app/components/worker-card/worker-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-worker-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./worker-card.component.css']
 })
 export class WorkerCardComponent implements OnInit {
+
+  @Input() name = "Kasia";/*interfejs wejscia dla komponentu, jesli nic nie przekaze bedzie kasia jak przekaze to bedzie przekazane*/
 
   constructor() { }
 
