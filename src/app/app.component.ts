9f2179d9737482738ca932f6f0c1b9b94ff75c99
rzeznik-kasia <rzeznik.kasia@gmail.com>
2017-08-30 11:27:30 +0200

    obiekt jsonowo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,13 @@ export class AppComponent {
   today: number = Date.now();
   sum: number[] = [1, 4, 7];
   access: boolean = true;
+  items: any = [
+    {name: "Joe", phone: 12454},
+    {name: "Mike", phone: 15454},
+    {name: "Bob", phone: 45},
+    {name: "Joe", phone: 123554},
+    {name: "Bożena", phone: 144234},
+    {name: "Ola", phone: 123433},
+    {name: "Kasia", phone: 122234},
+  ];
 }
