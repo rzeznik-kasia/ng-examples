ed788392e952495cc567f02c35b167fe4964a1c1
rzeznik-kasia <rzeznik.kasia@gmail.com>
2017-08-30 13:38:13 +0200

    add service

---
+++ src/app/services/workers.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {Http} from "@angular/http/src";
+
+@Injectable()
+export class WorkersService {
+
+  constructor(private http:Http) { }
+
+  getWorkersFromServer(): Observable<any> {
+    return this.http.get('https://emitter.pl/api/workers');
+  }
+
+}
